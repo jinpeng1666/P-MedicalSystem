ef21cb854852ec392e51904ed69e5622f9c8f6b7
JP.Zhou <3214423826@qq.com>
2024-11-05 19:18:27 +0800

    chore: 集成element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// https://vite.dev/config/
+
+// 集成element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 集成element-plus
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // 相对路径别名配置，使用 @ 代替 src
