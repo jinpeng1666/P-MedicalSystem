7298f319e2e374a4fed1043da7b4df9f1f3ed590
JP.Zhou <3214423826@qq.com>
2024-11-06 10:36:12 +0800

    chore: 集成pinia

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@/styles/index.scss'
+// 样式，按需引入element相关组件
+import 'element-plus/dist/index.css'
+import { ElButton } from 'element-plus'
+// 引入pinia
+import pinia from './store'
+
 const app = createApp(App)
+
+// 使用element相关组件
+app.use(ElButton)
+//使用pinia
+app.use(pinia)
+
 app.mount('#app')
