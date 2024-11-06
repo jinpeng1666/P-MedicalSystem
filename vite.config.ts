aeca34d4e635c536d30d4ff7c306dea9a1f9a99d
JP.Zhou <3214423826@qq.com>
2024-11-06 11:19:17 +0800

    chore: 配置mock

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,29 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    // 集成element-plus
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 相对路径别名配置，使用 @ 代替 src
+// 集成mock
+import { viteMockServe } from 'vite-plugin-mock'
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      // 集成element-plus
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      // 集成mock
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // 相对路径别名配置，使用 @ 代替 src
+      },
     },
-  },
+  }
 })
