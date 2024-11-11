72bee14616a3b7b014f18093df921bb4b11996bc
JP.Zhou <3214423826@qq.com>
2024-11-11 16:47:58 +0800

    feat: 路由骨架搭建

---
+++ src/router/index.ts
@@ -0,0 +1,76 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { constantRouterMap } from './routes'
+
+// 引入nprogress
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 免登录白名单
+const whiteList = ['/login', '/404']
+
+// 引入user仓库
+import useUserStore from '@/store/modules/user'
+
+// 引入permission方法
+import { hasPermission } from '@/utils/permission'
+
+// 创建路由
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: constantRouterMap,
+  scrollBehavior() {
+    return {
+      left: 0,
+      top: 0,
+    }
+  },
+})
+
+// 全局前置路由
+router.beforeEach(async (to, from, next) => {
+  const userStore = useUserStore()
+  nprogress.start()
+  if (userStore.token) {
+    // 第一层判断：token值存在
+    if (userStore.avatar === '' || userStore.userName === '') {
+      // 第二层判断：info未获取
+      await userStore.userMessage()
+    }
+    if (to.path === '/login') {
+      // 第三层判断：路由路径是登陆页面路径
+      next('/')
+    } else {
+      // 第三层判断：路由路径不是登陆页面路径
+      if (to.meta && to.meta.role) {
+        // 第四层判断：页面需要权限
+        if (hasPermission(userStore.roles, to)) {
+          // 第五层：有权限
+          next()
+        } else {
+          // 第五层判断：无权限
+          next('/404')
+        }
+      } else {
+        // 第三层判断：页面不需要权限
+        next()
+      }
+    }
+  } else {
+    // 第一层判断：token值不存在
+    if (whiteList.includes(to.path)) {
+      // 第二层判断：路由路径在免登录白名单上
+      next()
+    } else {
+      // 第二层登录：登录路径不在免登录白名单上
+      next('/login')
+    }
+  }
+})
+
+// 全局后置路由
+router.afterEach(() => {
+  nprogress.done()
+})
+
+// 导出router
+export default router
---
+++ src/router/routes.ts
@@ -0,0 +1,72 @@
+// 存放所有权限通用路由表
+
+export const constantRouterMap = [
+  // 登录路由
+  {
+    path: '/login',
+    component: () => import('@/views/login/index.vue'),
+    name: 'login',
+  },
+  // 首页路由
+  {
+    path: '/',
+    component: () => import('@/layout/index.vue'),
+    name: 'layout',
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        component: () => import('@/views/home/index.vue'),
+      },
+    ],
+  },
+  // 404路由
+  {
+    path: '/404',
+    component: () => import('@/views/404/index.vue'),
+    name: '404',
+  },
+  // 其他不存在路由
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404',
+    name: 'any',
+  },
+]
+
+// 存放需要根据权限动态加载的路由表
+export const asyncRouterMap = [
+  // {
+  //   path: '/',
+  //   component: () => import('@/layout/index.vue'),
+  //   name: 'layout',
+  //   redirect: '/home',
+  //   children: [
+  //     {
+  //       path: '/employee',
+  //       component: () => import('@/views/employee/index.vue'),
+  //       meta: { role: ['Manager', 'Minister'] },
+  //     },
+  //     {
+  //       path: '/finance',
+  //       component: () => import('@/views/finance/index.vue'),
+  //       meta: { role: ['Manager', 'Finance'] },
+  //     },
+  //     {
+  //       path: '/attendance',
+  //       component: () => import('@/views/attendance/index.vue'),
+  //       meta: { role: ['Manager', 'Attendance'] },
+  //     },
+  //     {
+  //       path: '/operation',
+  //       component: () => import('@/views/operation/index.vue'),
+  //       meta: { role: ['Manager', 'Operation'] },
+  //     },
+  //     {
+  //       path: '/authority',
+  //       component: () => import('@/views/authority/index.vue'),
+  //       meta: { role: ['Manager'] },
+  //     },
+  //   ],
+  // },
+]
