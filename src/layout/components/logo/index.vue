43b9339fb179a1e3a45a9ca0ea7e4282143c1498
JP.Zhou <3214423826@qq.com>
2024-11-17 16:43:10 +0800

    feat: 布局初步搭建

---
+++ src/layout/components/logo/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row class="logo">
+    <el-col :span="8" class="logo-container">
+      <!-- logo -->
+      <svg class="logo-svg">
+        <use xlink:href="#icon-logo"></use>
+      </svg>
+    </el-col>
+    <el-col :span="16" class="title-container">
+      <!-- title -->
+      <span>{{ title }}</span>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+let title = ref(import.meta.env.VITE_APP_TITLE)
+</script>
+
+<style scoped lang="scss">
+// 导入全局样式
+@use '@/styles/variable.scss' as variable;
+// logo所在的el-col的span等于($logo-height/$el-aside-width) * 24
+
+.logo {
+  height: variable.$logo-height;
+  // logo容器
+  .logo-container {
+    height: 100%;
+    // 图标容器样式
+    .logo-svg {
+      width: variable.$logo-height;
+      height: variable.$logo-height;
+    }
+  }
+  // title容器
+  .title-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    // title样式
+    span {
+      font-size: 20px;
+      font-weight: 700;
+    }
+  }
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- 总容器 -->
+  <el-container class="total-container">
+    <!-- 左侧导航栏 -->
+    <el-aside class="el-aside">
+      <Logo></Logo>
+    </el-aside>
+    <!-- 右侧容器 -->
+    <el-container>
+      <!-- 右侧容器面包屑 -->
+      <el-header>头部</el-header>
+      <!-- 右侧容器内容展示区 -->
+      <el-main>
+        <!-- 二级路由展示 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+// 引入组件
+import Logo from './components/logo/index.vue'
+</script>
+
+<style scoped lang="scss">
+// 导入全局样式
+@use '@/styles/variable.scss' as variable;
+// 总容器
+.total-container {
+  height: 100vh;
+  // 左侧导航栏
+  .el-aside {
+    width: variable.$el-aside-width;
+    background-color: variable.$el-aside-background;
+  }
+}
+</style>
