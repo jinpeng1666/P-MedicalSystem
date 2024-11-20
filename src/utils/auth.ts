50a5d3dd1ebf975ed608ec6527013fca97d4ac86
JP.Zhou <3214423826@qq.com>
2024-11-20 18:27:54 +0800

    style: 添加注解

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,31 @@
 import Cookies from 'js-cookie'
 
+/**
+ * 用于存储和获取用户身份的 Token 的键名
+ */
 const TokenKey = 'vue_medical_system_token'
 
+/**
+ * 获取 Token
+ * @returns {string | undefined} - 返回存储在 Cookie 中的 Token 值，如果不存在则返回 undefined
+ */
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
-export function setToken(token: any) {
+/**
+ * 设置 Token
+ * @param {string} token - 要存储的 Token 值
+ * @returns {Cookies.CookiesStatic} - 返回 js-cookie 的操作实例，用于链式调用
+ */
+export function setToken(token: string) {
   return Cookies.set(TokenKey, token)
 }
 
+/**
+ * 删除 Token
+ * @returns {void} - 不返回任何值
+ */
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,15 +1,17 @@
 /**
- * 对axios进行二次封装
+ * 对 axios 进行二次封装
+ * 主要目的是配置基础路径、统一处理请求拦截、响应拦截等功能。
  */
 
 import axios from 'axios'
 
-// 引入element-plus的组件
+// 引入 Element Plus 的消息提示组件，用于显示错误信息
 import { ElMessage } from 'element-plus'
 
-// 引入user仓库，判断是否包含token
+// 引入 user 仓库，用于获取用户的 token 信息
 import useUserStore from '@/store/modules/user'
 
+// 创建 axios 实例，配置基础路径和超时时间
 const request = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
   timeout: 5000,
